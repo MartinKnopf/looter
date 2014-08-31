6354863002cf080676f5f78a292374939b2fe6f3
Horsed <legendaryhorsed@gmail.com>
2014-08-31 21:43:36 +0200

    balance calculation (finally)

--- looter.js
+++ looter.js
@@ -17,42 +17,50 @@
     , clickBuy = Rx.Observable.fromEvent($('.stock1 .buy'), 'click')
     , clickSell = Rx.Observable.fromEvent($('.stock1 .sell'), 'click')
 
-  var turns = Rx.Observable
-    .merge(clickNextTurn, clickBuy, clickSell)
+  var nextTurn = clickNextTurn
     .select(function(event, idx) { return idx + 1; })
     .take(10)
     .repeat()
 
-  var randSinus = turns
+  var newPrice = nextTurn
     .map(_.partial(sinPrice, stocks[0].min, stocks[0].max))
     .map(_.partial(randomize, stocks[0].min, stocks[0].max))
     .publish()
 
+  var newBuy = Rx.Observable
+    .combineLatest(clickBuy, newPrice, function(event, price) { return {event: event, price: price}; })
+    .distinctUntilChanged(function(x) { return x.event; })
+    .map(function(x) { return -x.price; })
+
+  var newSell = Rx.Observable
+    .combineLatest(clickSell, newPrice, function(event, price) { return {event: event, price: price}; })
+    .distinctUntilChanged(function(x) { return x.event; })
+    .map(function(x) { return x.price; })
+
   var balance = Rx.Observable
-    .return(10000)
-  balance = clickBuy
-    .map(function(b, e, r, l) {
-        return ;
-      })
-    .startWith(10000)
+    .merge(Rx.Observable.return(10000), newBuy, newSell)
+    .scan(function(sum, price) { return sum + price; })
+    .filter(function(balance) { return balance >= 0; })
 
   // observers
 
-  turns.subscribe(function(turn) {
+  nextTurn.subscribe(function(turn) {
     $('.turn-nr').text(turn);
+    $('.stock1 .buy').prop('disabled', false);
+    $('.stock1 .sell').prop('disabled', false);
   })
 
   clickBuy.subscribe(function() {
     $('.stock1 .buy').prop('disabled', true);
-    $('.stock1 .sell').prop('disabled', false);
+    $('.stock1 .sell').prop('disabled', true);
   })
 
   clickSell.subscribe(function() {
     $('.stock1 .sell').prop('disabled', true);
-    $('.stock1 .buy').prop('disabled', false);
+    $('.stock1 .buy').prop('disabled', true);
   })
 
-  randSinus.subscribe(function(price) {
+  newPrice.subscribe(function(price) {
     $('.stock1 .price').text(price);
   })
 
@@ -60,6 +68,6 @@
     $('.balance').text(balance);
   })
 
-  randSinus.connect()
+  newPrice.connect()
 
 }(Rx, moment, _))
